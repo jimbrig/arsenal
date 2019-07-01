8d4b7bbca9188725845f8a1648f466dccda2ccb0
jimbrig2011 <jimmy.briggs@oliverwyman.com>
2019-07-01 14:28:59 -0400

    test

--- tests/testthat/test-dates.R
+++ tests/testthat/test-dates.R
@@ -5,3 +5,7 @@ library(arsenal)
 test_that("end of month is correct (i.e. 30 versus 31 and 28 for February)", {
   expect_equal(end_of_month(as.Date("2019-02-05")), as.Date("2019-02-28"))
 })
+
+test_that("start of month is correct", {
+  expect_equal(start_of_month(as.Date("2015-07-04")), as.Date("2015-07-01"))
+})
